cb3857dcf0fd1047a83bfdd412e6583570fc7bba
Riza Fahmi <rizafahmi@gmail.com>
2016-09-20 14:32:44 +0700

    Moving redux re-implementation to separate file

--- counter.js
+++ counter.js
@@ -11,30 +11,8 @@ const counter = (state = 0, action) => {
 
 }
 
-const createStore = (reducer) => {
 
-  let state
-  let listeners = []
-
-  const getState = () => state
-
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach(listener => listener())
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener)
-    return () => {
-      listeners.filter(l => l !== listener)
-    }
-  }
-
-  dispatch({})
-  return { getState, dispatch, subscribe }
-
-}
-
+import createStore from './store.js'
 const store = createStore(counter)
 
 const render = () => {
