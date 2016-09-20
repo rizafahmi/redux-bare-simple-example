9f60d8b63d8aeeb70b0d9e60e325a5a9b6cbbc4c
Riza Fahmi <rizafahmi@gmail.com>
2016-09-20 14:30:30 +0700

    Re-implementing redux from scratch!

--- counter.js
+++ counter.js
@@ -11,7 +11,30 @@ const counter = (state = 0, action) => {
 
 }
 
-const { createStore } = Redux
+const createStore = (reducer) => {
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({})
+  return { getState, dispatch, subscribe }
+
+}
+
 const store = createStore(counter)
 
 const render = () => {
